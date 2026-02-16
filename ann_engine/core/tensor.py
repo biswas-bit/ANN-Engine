2163c60b9d25a109416233c9766f228bbd2e1d27
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-16 11:05:43 +0545

    made tensor more dynamic

--- ann_engine/core/tensor.py
+++ ann_engine/core/tensor.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 class Tensor:
-    def __init__(self,data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op=""):
         self.data = np.array(data, dtype=np.float32)
+        self.grad = 0.0
         self._prev = set(_children)
         self._op = _op
     
-    def __add__(self,other):
-        if isinstance(other,Tensor):
-            return Tensor(self.data + other.data, (self, other), '+')
+    def __add__(self, other):
+        other = other if isinstance(other,Tensor) else Tensor(other)
+        return Tensor(self.data+other.data, (self, other), '+')
     
     def __mul__(self, other):
-        if isinstance(other, Tensor):
-            return Tensor(self.data * other.data, (self, other), '*')
+        other = other if isinstance(other,Tensor) else Tensor(other)
+        return Tensor(self.data*other.data, (self, other),"*")
     
     def __repr__(self):
         return f"Tensor(data={self.data})"
