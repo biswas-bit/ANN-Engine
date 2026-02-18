f173213044ae9f5b7c6ddf1c1524f42d319dc8ea
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-18 20:43:04 +0545

    test case

--- test/test.py
+++ test/test.py
@@ -1,13 +1,20 @@
-from ann_engine.core.tensor import Tensor
+from ann_engine.core.parameter import Parameter
+import numpy as np
 
-a = Tensor(2.0)
-b = Tensor(3.0)
-c = Tensor(4.0)
-d = a*b-c*b-a
+W = Parameter(np.array([[1., 2.],
+                        [3., 4.]]))
+print("Data:\n", W.data)
+print("Grad:\n", W.grad)
+print("Required Grad:", W.required_grad)
 
-d.backward()
+x = Parameter(np.array([[1., 1.],
+                        [0., 1.]]))
+z= W + x
+print("\nResult of w+x (Tensor):",z)
+print("z is Parameter? :", isinstance(z, Parameter))
+print("\nz.data:\n", z.data)
 
-print("d:", d)
-print("a.grad:", a.grad) 
-print("b.grad:", b.grad)  
-print("c.grad:", c.grad)  
+z.backward()
+print("\ngrad after backward:")
+print("\nGrad of W:\n", W.grad)
+print("Grad of x:\n", x.grad)   
