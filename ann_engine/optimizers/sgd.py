bbc2e4d46f70b0c082fa90d4cd3f8fcd091604d6
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-19 13:45:53 +0545

    handaled shape

--- ann_engine/optimizers/sgd.py
+++ ann_engine/optimizers/sgd.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ann_engine.optimizers.base import Optimizer
 
 class SGD(Optimizer):
@@ -7,6 +8,16 @@ class SGD(Optimizer):
     def step(self):
         for param in self.parameters:
             if param.grad is not None:
+                # Ensure grad has the same shape as data
+                if param.grad.shape != param.data.shape:
+                    # If grad is scalar but data is not, expand grad
+                    if param.grad.shape == () and param.data.shape != ():
+                        param.grad = np.ones_like(param.data) * param.grad
+                    else:
+                        raise ValueError(f"Gradient shape {param.grad.shape} doesn't match data shape {param.data.shape}")
                 param.data -= self.lr * param.grad
                     
-        
+    def zero_grad(self):
+        for param in self.parameters:
+            # Reset gradients to zeros with the same shape as data
+            param.grad = np.zeros_like(param.data)
