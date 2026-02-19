6e0b573d8c1cd9d421b28a45963ed40a11770534
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-19 13:11:04 +0545

    updated

--- test/test.py
+++ test/test.py
@@ -5,7 +5,8 @@ import numpy as np
 class DummyLayer(Module):
     def __init__(self):
         super().__init__()
-        self.w = Parameter(np.array([[1.,2.],[3.,4]]))
+        self.w = Parameter(np.array([[1.,2.],
+                                     [3.,4]]))
         self.b = Parameter(np.array([1., 2.]))
 layer = DummyLayer()
 params = layer.parameters()
