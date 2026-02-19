3cf59b59aeeb260b84ab934d20df7b3ea72c5194
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-19 13:48:27 +0545

    created a base for opyimizers

--- ann_engine/optimizers/base.py
+++ ann_engine/optimizers/base.py
@@ -1,11 +1,15 @@
+from abc import ABC, abstractmethod
+
 class Optimizer:
     def __init__(self, parameters, lr=0.01):
         self.parameters = parameters
         self.lr = lr
-    
+        
+    @abstractmethod
     def step(self):
         raise NotImplementedError("This method should be implemented by subclasses")
     
+    @abstractmethod
     def zero_grad(self):
         for param in self.parameters:
             param.grad = 0 
