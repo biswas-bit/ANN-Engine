33f4c771cae0af91fa182803babdff1404c3b18e
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-19 13:04:36 +0545

    test case

--- test/test.py
+++ test/test.py
@@ -1,20 +1,12 @@
 from ann_engine.core.parameter import Parameter
+from ann_engine.layers.base import Module
 import numpy as np
 
-W = Parameter(np.array([[1., 2.],
-                        [3., 4.]]))
-print("Data:\n", W.data)
-print("Grad:\n", W.grad)
-print("Required Grad:", W.required_grad)
-
-x = Parameter(np.array([[1., 1.],
-                        [0., 1.]]))
-z= W + x
-print("\nResult of w+x (Tensor):",z)
-print("z is Parameter? :", isinstance(z, Parameter))
-print("\nz.data:\n", z.data)
-
-z.backward()
-print("\ngrad after backward:")
-print("\nGrad of W:\n", W.grad)
-print("Grad of x:\n", x.grad)   
+class DummyLayer(Module):
+    def __init__(self):
+        super().__init__()
+        self.w = Parameter(np.array([[1.,2.],[3.,4]]))
+        self.b = Parameter(np.array([1., 2.]))
+layer = DummyLayer()
+params = layer.parameters()
+print(params)
