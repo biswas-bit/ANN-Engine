82ad9b20b5338a9045277a28918fb5ae774f86bb
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-20 21:22:06 +0545

    runned test case

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from ann_engine.core.parameter import Parameter
-from ann_engine.optimizers.sgd import SGD
+from ann_engine.optimizers.sgd import SGD, SGDWithMomentum
 
 
 W = Parameter(np.array([[2.0, 3.0],
@@ -17,8 +17,12 @@ print("W.grad:\n", W.grad)
 
 
 optimizer = SGD([W], lr=0.1)
+optimizer2 = SGDWithMomentum([W], lr=0.1, momentum=0.9)
 
 optimizer.step()
+optimizer2.step()
 
 print("\nAfter step:")
 print("W.data:\n", W.data)
+
+input()
