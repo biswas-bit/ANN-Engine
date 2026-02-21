d460cded7ff659170feeb9dc18ec528adfce24ea
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-21 21:22:14 +0545

    updated SGD with momentum with state_dict and loading

--- ann_engine/optimizers/sgd.py
+++ ann_engine/optimizers/sgd.py
@@ -32,6 +32,7 @@ class SGD(Optimizer):
         """Load optimizer state from a state_dict"""
         hyperparams = state_dict['hyperparameters']
         self.lr = hyperparams['lr']
+        
             
             
 class SGDWithMomentum(SGD):
@@ -56,3 +57,21 @@ class SGDWithMomentum(SGD):
 
     def zero_grad(self):
         super().zero_grad()
+        
+    def state_dict(self):
+        """Return optimizer state for saving/loading"""
+        return {
+            'velocities': self.velocities,
+            'hyperparameters': {
+                'lr': self.lr,
+                'momentum': self.momentum
+            }
+        }
+        
+    def load_state_dict(self, state_dict):
+        """Load optimizer state from a state_dict"""
+        self.velocities = state_dict['velocities']
+        hyperparams = state_dict['hyperparameters']
+        self.lr = hyperparams['lr']
+        self.momentum = hyperparams['momentum']
+        
