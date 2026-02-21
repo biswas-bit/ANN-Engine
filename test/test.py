db4741a44a63657eb6497df555dbf7610625f1db
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-21 19:26:41 +0545

    created a test

--- test/test.py
+++ test/test.py
@@ -1,28 +1,16 @@
-import numpy as np
 from ann_engine.core.parameter import Parameter
-from ann_engine.optimizers.sgd import SGD, SGDWithMomentum
+from ann_engine.optimizers.nag import NAG
+import numpy as np
 
+w = Parameter(np.array([0.0]))
+optimizers = NAG([w], lr=0.1, momentum=0.9)
 
-W = Parameter(np.array([[2.0, 3.0],
-                        [4.0, 5.0]]))
-
-loss = (W * 2).sum()
-
-
-loss.backward()
-
-print("Before step:")
-print("W.data:\n", W.data)
-print("W.grad:\n", W.grad)
-
-
-optimizer = SGD([W], lr=0.1)
-optimizer2 = SGDWithMomentum([W], lr=0.1, momentum=0.9)
-
-optimizer.step()
-optimizer2.step()
-
-print("\nAfter step:")
-print("W.data:\n", W.data)
+for step in range(5):
+    optimizers.zero_grad()
+    loss = (w-5)**2
+    loss.backward()
+    optimizers.step()
+    print("grad:",w.grad)
+    print(f"step {step}:w={w.data} loss={loss.data}")
 
 input()
