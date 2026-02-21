4a5c9dbbae6c268ea6b8c479749034651fc801ba
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-21 20:02:23 +0545

    test case running correctly

--- test/test.py
+++ test/test.py
@@ -1,18 +1,27 @@
 from ann_engine.core.parameter import Parameter
-from ann_engine.optimizers.nag import NAG
+from ann_engine.optimizers.adagrad import AdaGrad
 import numpy as np
 
+# Parameter
 w = Parameter(np.array([0.0]))
-optimizers = NAG([w], lr=0.1, momentum=0.9)
-print(f"w:{w.grad}")
-print(f"optimizers velocity:{optimizers. velocities[0]}")
 
+# Optimizer
+optimizer = AdaGrad([w], lr=0.1)
+
+# Training loop
 for step in range(5):
-    optimizers.zero_grad()
-    loss = (w-5)**2
-    loss.backward()
-    optimizers.step()
-    print("grad:",w.grad)
-    print(f"step {step}:w={w.data} loss={loss.data}")
+    optimizer.zero_grad()
 
+    # Forward
+    loss = (w - 5) ** 2
+    loss.backward()
+
+    # Print before step
+    print(f"Step {step} before update: w={w.data}, grad={w.grad}")
+
+    # Update
+    optimizer.step()
+
+    # Print after step
+    print(f"Step {step} after update: w={w.data}")
 input()
