51f7a61dc71f7e1f25ef8a4f15c4a0b1fb4cb73c
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-22 20:16:25 +0545

    tested for mse

--- test/test.py
+++ test/test.py
@@ -1,16 +1,20 @@
-from ann_engine.losses.loss import CrossEntropyLoss 
-from ann_engine.core import Tensor
+from ann_engine.losses.loss import MSELoss 
+from ann_engine.core import Tensor, Parameter
 
+# Regression data
 y_true = Tensor([1.0, 2.0, 3.0])
-y_pred = Tensor([1.5, 2.5, 3.5])
+y_pred = Parameter([1.5, 2.5, 3.5])  # Use Parameter for gradients
 
-try :
-  loss_fn = CrossEntropyLoss(reduction='mean')
-  loss = loss_fn(y_pred, y_true)
-  print("Cross Entropy Loss:", loss.data)
+try:
+    loss_fn = MSELoss(reduction='mean')
+    loss = loss_fn(y_pred, y_true)
+    print("MSE Loss:", loss.data)
 
-  loss.backward()
-  print("Gradient w.r.t y_pred:", y_pred.grad.data)
+    loss.backward()
+    print("Gradient w.r.t y_pred:", y_pred.grad)
 except Exception as e:
-  print("Error during loss computation or backpropagation:", e)
-input("press enter to continue..")
+    print("Error during loss computation or backpropagation:", e)
+    import traceback
+    traceback.print_exc()
+
+input("Press Enter to continue...")
