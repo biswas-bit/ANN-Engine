0745e7067b8dc22652eff9ff2f58627c90693b9b
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-22 13:15:14 +0545

    created a test case for cross

--- test/test.py
+++ test/test.py
@@ -1,13 +1,13 @@
-from ann_engine.losses.loss import MSELoss 
+from ann_engine.losses.loss import CrossEntropyLoss 
 from ann_engine.core import Tensor
 
 y_true = Tensor([1.0, 2.0, 3.0])
 y_pred = Tensor([1.5, 2.5, 3.5])
 
 try :
-  loss_fn = MSELoss(reduction='mean')
+  loss_fn = CrossEntropyLoss(reduction='mean')
   loss = loss_fn(y_pred, y_true)
-  print("MSE Loss:", loss.data)
+  print("Cross Entropy Loss:", loss.data)
 
   loss.backward()
   print("Gradient w.r.t y_pred:", y_pred.grad.data)
