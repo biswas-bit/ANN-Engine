e2cde02e7e46a0b5b86aabe28efdd20754778785
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-22 20:43:33 +0545

    created a base for layes

--- ann_engine/layers/base.py
+++ ann_engine/layers/base.py
@@ -1,31 +1,31 @@
 class Module:
-    """
-    Base class for all layers.
-    Holds trainable Parameters.
-    """
+    """ Base Class for all neural network modules """
+    
     def __init__(self):
         self._parameters = {}
-
-    def forward(self, *inputs):
-        """
-        Define in child class.
-        """
-        raise NotImplementedError
-
+        self._modules = {}
+        
+    def forward(self,*inputs):
+        raise NotImplementedError("Forward method not implemented")
+    
+    def __call__(self, *inputs):
+        return self.forward(*inputs)
+    
     def __setattr__(self, name, value):
-        """
-        Override setattr to track Parameters automatically.
-        """
         from ann_engine.core.parameter import Parameter
-
+        from ann_engine.layers.base import Module
+        
         if isinstance(value, Parameter):
-            if not hasattr(self, "_parameters"):
-                super().__setattr__("_parameters", {})
             self._parameters[name] = value
+        
+        elif isinstance(value, Module):
+            self._modules[name] = value
+            
         super().__setattr__(name, value)
-
+        
     def parameters(self):
-        """
-        Return all Parameters of this module.
-        """
-        return list(self._parameters.values())
+        params = list(self._parameters.values())
+        for module in self._modules.values():
+            params.extend(module.parameters())
+        
+        return params
