1bd14b3f558646245aa9af74f6a369d68f9148ff
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-24 11:31:09 +0545

    every thing is correct about sigmoid (working well)

--- test/test.py
+++ test/test.py
@@ -1,18 +1,19 @@
 import numpy as np
-from ann_engine.layers import ReLU
+from ann_engine.layers import ReLU, Sigmoid
 from ann_engine.core import Tensor
 
-relu = ReLU()
+sig = Sigmoid()
 try:
-  input_for_relu = Tensor(np.array([1.2,1.3, 1.4, 1.5]))
+  input_for_sig = Tensor(np.array([1.2,1.3, 1.4, 1.5]))
 except Exception as e:
     print(f"error occoured : {e}")
+    
 try:
-  print(relu.forward(input_for_relu))
-  input_for_relu.backward()
-  print(f" gradient for relu Activation: {input_for_relu.grad}")
-  print(f" parent note for gradient : {input_for_relu._prev}")
-  print(f" Operator for Relu in computation graph: {input_for_relu._op} ")
+  print(sig.forward(input_for_sig))
+  input_for_sig.backward()
+  print(f" gradient for relu Activation: {input_for_sig.grad}")
+  print(f" parent note for gradient : {input_for_sig._prev}")
+  print(f" Operator for Relu in computation graph: {input_for_sig._op} ")
 except Exception as e:
     print(f"error occoured: {e}")
     
