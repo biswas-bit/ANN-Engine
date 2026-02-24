c85a56bd05fae882f38a4bfdeba3794c71320d72
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-24 11:38:41 +0545

    tested for leakyrelu

--- test/test.py
+++ test/test.py
@@ -1,19 +1,19 @@
 import numpy as np
-from ann_engine.layers import ReLU, Sigmoid, Tanh
+from ann_engine.layers import ReLU, Sigmoid, Tanh, LeakyReLU
 from ann_engine.core import Tensor
 
-tan = Tanh()
+Lr = LeakyReLU(alpha=0.10)
 try:
-  input_for_tan = Tensor(np.array([1.2,1.3, 1.4, 1.5]))
+  input_for_lr = Tensor(np.array([1.2,1.3, 1.4, 1.5]))
 except Exception as e:
     print(f"error occoured : {e}")
     
 try:
-  print(tan.forward(input_for_tan))
-  input_for_tan.backward()
-  print(f" gradient for relu Activation: {input_for_tan.grad}")
-  print(f" parent note for gradient : {input_for_tan._prev}")
-  print(f" Operator for Relu in computation graph: {input_for_tan._op} ")
+  print(Lr.forward(input_for_lr))
+  input_for_lr.backward()
+  print(f" gradient for relu Activation: {input_for_lr.grad}")
+  print(f" parent note for gradient : {input_for_lr._prev}")
+  print(f" Operator for Relu in computation graph: {input_for_lr._op} ")
 except Exception as e:
     print(f"error occoured: {e}")
     
