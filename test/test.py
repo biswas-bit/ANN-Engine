c98baedf2d25f66afeb8ce95728ee08f83f6e40b
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-24 11:35:33 +0545

    tested for tanh

--- test/test.py
+++ test/test.py
@@ -1,19 +1,19 @@
 import numpy as np
-from ann_engine.layers import ReLU, Sigmoid
+from ann_engine.layers import ReLU, Sigmoid, Tanh
 from ann_engine.core import Tensor
 
-sig = Sigmoid()
+tan = Tanh()
 try:
-  input_for_sig = Tensor(np.array([1.2,1.3, 1.4, 1.5]))
+  input_for_tan = Tensor(np.array([1.2,1.3, 1.4, 1.5]))
 except Exception as e:
     print(f"error occoured : {e}")
     
 try:
-  print(sig.forward(input_for_sig))
-  input_for_sig.backward()
-  print(f" gradient for relu Activation: {input_for_sig.grad}")
-  print(f" parent note for gradient : {input_for_sig._prev}")
-  print(f" Operator for Relu in computation graph: {input_for_sig._op} ")
+  print(tan.forward(input_for_tan))
+  input_for_tan.backward()
+  print(f" gradient for relu Activation: {input_for_tan.grad}")
+  print(f" parent note for gradient : {input_for_tan._prev}")
+  print(f" Operator for Relu in computation graph: {input_for_tan._op} ")
 except Exception as e:
     print(f"error occoured: {e}")
     
