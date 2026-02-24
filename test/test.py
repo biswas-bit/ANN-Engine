873b3f71c1ac39b47a71948f2997f8d5fe575af4
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-24 11:44:48 +0545

    tested for softplus

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 import numpy as np
-from ann_engine.layers import ReLU, Sigmoid, Tanh, LeakyReLU, ELU, Softmax
+from ann_engine.layers import ReLU, Sigmoid, Tanh, LeakyReLU, ELU, Softmax, LogSoftmax, Softplus
 from ann_engine.core import Tensor
 
-Lr = Softmax(dim=-1)
+Lr = Softplus()
 try:
   input_for_lr = Tensor(np.array([1.2,1.3, 1.4, 1.5]))
 except Exception as e:
