e6dcf6dfe4ac15fb08d09a0ce11423f5b0c968a8
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-26 19:43:22 +0545

    updated cause there was import error

--- ann_engine/optimizers/__init__.py
+++ ann_engine/optimizers/__init__.py
@@ -1,15 +1,15 @@
-from ann_engine.optimizers.adagrad import Adagrad
+from ann_engine.optimizers.adagrad import AdaGrad
 from ann_engine.optimizers.adam import Adam
 from ann_engine.optimizers.rsmprop import RMSProp
-from ann_engine.optimizers.sgd import SGD, SGDMomentum
+from ann_engine.optimizers.sgd import SGD,SGDWithMomentum
 from ann_engine.optimizers.nag import NAG
 
 __all__ = [
     "SGD",
-    "SGDMomentum", 
+    "SGDWithMomentum", 
     "NAG",
-    "Adagrad",
+    "AdaGrad",
     "Adam",
-    "RMSProp"
+    "RMSProp",
 ]
 
