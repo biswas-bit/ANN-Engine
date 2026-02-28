c5e9c15274efb764701b499848553c6a5fa47832
biswas-bit <pokhrelbiswas53@gmail.com>
2026-02-28 12:50:19 +0545

    tensor is not creating computation graph(main issue)

--- ann_engine/core/tensor.py
+++ ann_engine/core/tensor.py
@@ -1,13 +1,18 @@
 import numpy as np
 
+
 class Tensor:
     def __init__(self, data, _children=(), _op=''):
         self.data = np.array(data, dtype=np.float32)
         self.grad = np.zeros_like(self.data)
         self._prev = set(_children)
         self._op = _op
-        self._backward = lambda: None 
-        
+        self._backward = lambda: None
+
+    # ------------------------------------------------------------------
+    # Reduction ops
+    # ------------------------------------------------------------------
+
     def sum(self):
         out = Tensor(self.data.sum(), (self,), 'sum')
         saved_self = self
@@ -15,13 +20,15 @@ class Tensor:
             saved_self.grad += out.grad * np.ones_like(saved_self.data)
         out._backward = _backward
         return out
-     
+
     def mean(self, axis=None, keepdims=False):
         if axis is None:
             out = Tensor(self.data.mean(), (self,), 'mean')
             saved_self = self
             def _backward():
-                saved_self.grad += np.ones_like(saved_self.data) * (out.grad / saved_self.data.size)
+                saved_self.grad += (
+                    np.ones_like(saved_self.data) * (out.grad / saved_self.data.size)
+                )
             out._backward = _backward
             return out
         else:
@@ -33,12 +40,17 @@ class Tensor:
                 grad = out.grad / saved_self.data.shape[saved_axis]
                 expand_shape = list(saved_self.data.shape)
                 if not saved_keepdims:
-                    expand_shape.insert(saved_axis if saved_axis >= 0 else len(expand_shape), 1)
+                    insert_at = saved_axis if saved_axis >= 0 else len(expand_shape)
+                    expand_shape.insert(insert_at, 1)
                 grad = grad.reshape(expand_shape)
                 saved_self.grad += np.broadcast_to(grad, saved_self.data.shape)
             out._backward = _backward
             return out
-    
+
+    # ------------------------------------------------------------------
+    # Shape ops
+    # ------------------------------------------------------------------
+
     def reshape(self, *shape):
         out = Tensor(self.data.reshape(shape), (self,), 'reshape')
         saved_self = self
@@ -46,76 +58,68 @@ class Tensor:
             saved_self.grad += out.grad.reshape(saved_self.data.shape)
         out._backward = _backward
         return out
-    
+
     @property
     def T(self):
         out = Tensor(self.data.T, (self,), 'T')
         saved_self = self
         def _backward():
-            saved_self.grad += out.grad.T 
+            saved_self.grad += out.grad.T
         out._backward = _backward
         return out
+
+    # ------------------------------------------------------------------
+    # Arithmetic ops
+    # ------------------------------------------------------------------
 
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
         saved_self = self
         saved_other = other
+
         def _backward():
-            if saved_self.data.shape != out.data.shape:
-                axis_to_sum = []
-                for i, (dim_self, dim_out) in enumerate(zip(saved_self.data.shape, out.data.shape)):
-                    if dim_self != dim_out:
-                        axis_to_sum.append(i)
-                if axis_to_sum:
-                    grad_self = out.grad.sum(axis=tuple(axis_to_sum), keepdims=True)
-                    grad_self = grad_self.reshape(saved_self.data.shape)
-                else:
-                    grad_self = out.grad
-            else:
-                grad_self = out.grad
+            # Handle broadcast: sum over axes that were broadcast
+            def _reduce_grad(grad, target_shape):
+                # Sum over leading dims if grad has more dims
+                while grad.ndim > len(target_shape):
+                    grad = grad.sum(axis=0)
+                # Sum over axes where target has size 1
+                for i, dim in enumerate(target_shape):
+                    if dim == 1:
+                        grad = grad.sum(axis=i, keepdims=True)
+                return grad
 
-            if saved_other.data.shape != out.data.shape:
-                axis_to_sum = []
-                for i, (dim_other, dim_out) in enumerate(zip(saved_other.data.shape, out.data.shape)):
-                    if dim_other != dim_out:
-                        axis_to_sum.append(i)
-                if axis_to_sum:
-                    grad_other = out.grad.sum(axis=tuple(axis_to_sum), keepdims=True)
-                    grad_other = grad_other.reshape(saved_other.data.shape)
-                else:
-                    grad_other = out.grad
-            else:
-                grad_other = out.grad
+            saved_self.grad  += _reduce_grad(out.grad, saved_self.data.shape)
+            saved_other.grad += _reduce_grad(out.grad, saved_other.data.shape)
 
-            saved_self.grad += grad_self
-            saved_other.grad += grad_other
         out._backward = _backward
         return out
-    
+
     def __radd__(self, other):
         return self + other
-    
+
     def __sub__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data - other.data, (self, other), '-')
         saved_self = self
         saved_other = other
+
         def _backward():
-            if saved_self.data.shape != out.data.shape:
-                axis = tuple(range(len(out.grad.shape) - len(saved_self.data.shape)))
-                saved_self.grad += out.grad.sum(axis=axis)
-            else:
-                saved_self.grad += out.grad
-                
-            if saved_other.data.shape != out.data.shape:
-                axis = tuple(range(len(out.grad.shape) - len(saved_other.data.shape)))
-                saved_other.grad += (-out.grad).sum(axis=axis)
-            else:
-                saved_other.grad += -out.grad
+            def _reduce_grad(grad, target_shape):
+                while grad.ndim > len(target_shape):
+                    grad = grad.sum(axis=0)
+                for i, dim in enumerate(target_shape):
+                    if dim == 1:
+                        grad = grad.sum(axis=i, keepdims=True)
+                return grad
+
+            saved_self.grad  += _reduce_grad(out.grad, saved_self.data.shape)
+            saved_other.grad -= _reduce_grad(out.grad, saved_other.data.shape)
+
         out._backward = _backward
         return out
-    
+
     def __rsub__(self, other):
         return Tensor(other) - self
 
@@ -124,77 +128,81 @@ class Tensor:
         out = Tensor(self.data * other.data, (self, other), '*')
         saved_self = self
         saved_other = other
+
         def _backward():
-            grad_self = saved_other.data * out.grad
-            grad_other = saved_self.data * out.grad
-            
-            if saved_self.data.shape != grad_self.shape:
-                axis = tuple(range(len(grad_self.shape) - len(saved_self.data.shape)))
-                saved_self.grad += grad_self.sum(axis=axis)
-            else:
-                saved_self.grad += grad_self
-                
-            if saved_other.data.shape != grad_other.shape:
-                axis = tuple(range(len(grad_other.shape) - len(saved_other.data.shape)))
-                saved_other.grad += grad_other.sum(axis=axis)
-            else:
-                saved_other.grad += grad_other
+            grad_self  = saved_other.data * out.grad
+            grad_other = saved_self.data  * out.grad
+
+            def _reduce(grad, target_shape):
+                while grad.ndim > len(target_shape):
+                    grad = grad.sum(axis=0)
+                for i, dim in enumerate(target_shape):
+                    if dim == 1:
+                        grad = grad.sum(axis=i, keepdims=True)
+                return grad
+
+            saved_self.grad  += _reduce(grad_self,  saved_self.data.shape)
+            saved_other.grad += _reduce(grad_other, saved_other.data.shape)
+
         out._backward = _backward
         return out
-    
+
     def __rmul__(self, other):
         return self * other
-    
+
     def __truediv__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data / other.data, (self, other), '/')
         saved_self = self
         saved_other = other
+
         def _backward():
-            grad_self = (1.0 / saved_other.data) * out.grad
+            grad_self  = (1.0 / saved_other.data) * out.grad
             grad_other = (-saved_self.data / (saved_other.data ** 2)) * out.grad
-            
-            if saved_self.data.shape != grad_self.shape:
-                axis = tuple(range(len(grad_self.shape) - len(saved_self.data.shape)))
-                saved_self.grad += grad_self.sum(axis=axis)
-            else:
-                saved_self.grad += grad_self
-                
-            if saved_other.data.shape != grad_other.shape:
-                axis = tuple(range(len(grad_other.shape) - len(saved_other.data.shape)))
-                saved_other.grad += grad_other.sum(axis=axis)
-            else:
-                saved_other.grad += grad_other
+
+            def _reduce(grad, target_shape):
+                while grad.ndim > len(target_shape):
+                    grad = grad.sum(axis=0)
+                for i, dim in enumerate(target_shape):
+                    if dim == 1:
+                        grad = grad.sum(axis=i, keepdims=True)
+                return grad
+
+            saved_self.grad  += _reduce(grad_self,  saved_self.data.shape)
+            saved_other.grad += _reduce(grad_other, saved_other.data.shape)
+
         out._backward = _backward
         return out
-    
+
     def __rtruediv__(self, other):
         return Tensor(other) / self
-    
+
     def __pow__(self, power):
-        assert isinstance(power, (int, float)), "Only supports int or float powers"
+        assert isinstance(power, (int, float)), "Only int/float powers supported"
         out = Tensor(self.data ** power, (self,), f'**{power}')
-        saved_self = self
+        saved_self  = self
         saved_power = power
         def _backward():
-            saved_self.grad += (saved_power * saved_self.data ** (saved_power - 1)) * out.grad
+            saved_self.grad += (
+                saved_power * saved_self.data ** (saved_power - 1)
+            ) * out.grad
         out._backward = _backward
         return out
-    
+
     def __rpow__(self, other):
         raise NotImplementedError("Reverse power not implemented")
-    
+
     def __matmul__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data @ other.data, (self, other), '@')
-        saved_self = self
+        saved_self  = self
         saved_other = other
         def _backward():
-            saved_self.grad += out.grad @ saved_other.data.T
+            saved_self.grad  += out.grad @ saved_other.data.T
             saved_other.grad += saved_self.data.T @ out.grad
         out._backward = _backward
         return out
-    
+
     def __rmatmul__(self, other):
         return Tensor(other) @ self
 
@@ -206,106 +214,123 @@ class Tensor:
         out._backward = _backward
         return out
 
+    # ------------------------------------------------------------------
+    # Math ops
+    # ------------------------------------------------------------------
+
+    def log(self):
+        out = Tensor(np.log(self.data + 1e-8), (self,), 'log')   # +eps for stability
+        saved_self = self
+        def _backward():
+            saved_self.grad += out.grad / (saved_self.data + 1e-8)
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        out = Tensor(np.exp(self.data), (self,), 'exp')
+        saved_self = self
+        def _backward():
+            saved_self.grad += out.grad * out.data
+        out._backward = _backward
+        return out
+
+    def sqrt(self):
+        out = Tensor(np.sqrt(self.data), (self,), 'sqrt')
+        saved_self = self
+        def _backward():
+            saved_self.grad += out.grad * (0.5 / (out.data + 1e-8))
+        out._backward = _backward
+        return out
+
+    def square(self):
+        return self ** 2
+
+    def clip(self, min_val=None, max_val=None):
+        """Clip values — NOTE: gradients do not flow through clipped regions."""
+        clipped = np.clip(self.data, min_val, max_val)
+        out = Tensor(clipped, (self,), 'clip')
+        saved_self = self
+        def _backward():
+            # Pass gradient only where values were not clipped
+            mask = np.ones_like(saved_self.data)
+            if min_val is not None:
+                mask[saved_self.data < min_val] = 0.0
+            if max_val is not None:
+                mask[saved_self.data > max_val] = 0.0
+            saved_self.grad += out.grad * mask
+        out._backward = _backward
+        return out
+
+    # ------------------------------------------------------------------
+    # Comparison ops (no gradient — used for masking only)
+    # ------------------------------------------------------------------
+
+    def __gt__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return self.data > other.data
+
+    def __lt__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return self.data < other.data
+
+    # ------------------------------------------------------------------
+    # Backprop
+    # ------------------------------------------------------------------
+
     def backward(self, retain_graph=False):
-        topo = []
+        """
+        Reverse-mode autodiff via topological sort.
+
+        Args:
+            retain_graph: If True, keep the computation graph so backward()
+                          can be called again (e.g. for debugging).
+                          Default False — but we no longer destroy _prev
+                          to avoid breaking subsequent forward passes.
+        """
+        # Build topological order
+        topo    = []
         visited = set()
+
         def build_topo(t):
             if id(t) not in visited:
                 visited.add(id(t))
                 for child in t._prev:
                     build_topo(child)
                 topo.append(t)
+
         build_topo(self)
 
+        # Seed gradient
         if self.data.shape == ():
             self.grad = np.array(1.0, dtype=np.float32)
         else:
             self.grad = np.ones_like(self.data)
 
+        # Propagate gradients
         for t in reversed(topo):
             t._backward()
-            
-        if not retain_graph:
-            for t in topo:
-                t._prev = set()
+
+        # ── KEY FIX ──────────────────────────────────────────────────
+        # Do NOT clear _prev here.  Clearing _prev destroys the graph so
+        # the next forward pass produces Tensors whose ancestors are gone,
+        # making all subsequent backward() calls a no-op (zero gradients).
+        #
+        # Gradient accumulation is handled by zero_grad() in the optimizer,
+        # which resets .grad before each backward pass — that is correct and
+        # sufficient.  We never need to wipe _prev.
+        # ─────────────────────────────────────────────────────────────
 
     def zero_grad(self):
-        """Reset gradients to zero."""
+        """Reset this tensor's gradient to zero."""
         self.grad = np.zeros_like(self.data)
-        
-    
-    def log(self):
-        """
-        Natural logarithm (base e) of tensor elements
-        """
-        out = Tensor(np.log(self.data), (self,), 'log')
-        
-        def _backward():
-            self.grad += out.grad / self.data
-            
-        out._backward = _backward
-        return out
-    
-    def exp(self):
-        """
-        Exponential of tensor elements
-        """
-        out = Tensor(np.exp(self.data), (self,), 'exp')
-        
-        def _backward():
-            self.grad += out.grad * out.data
-            
-        out._backward = _backward
-        return out
-    
-    def __neg__(self):
-        """
-        Negation operator: -tensor
-        """
-        return self * -1
-    
+
     def __abs__(self):
-        """
-        Absolute value
-        """
         out = Tensor(np.abs(self.data), (self,), 'abs')
-        
+        saved_self = self
         def _backward():
-            self.grad += out.grad * np.sign(self.data)
-            
+            saved_self.grad += out.grad * np.sign(saved_self.data)
         out._backward = _backward
         return out
-    
-    def __gt__(self, other):
-        """Greater than comparison (for masking)"""
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        return self.data > other.data
-    
-    def __lt__(self, other):
-        """Less than comparison (for masking)"""
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        return self.data < other.data
-    
-    def sqrt(self):
-        """Square root"""
-        out = Tensor(np.sqrt(self.data), (self,), 'sqrt')
-        
-        def _backward():
-            self.grad += out.grad * (0.5 / out.data)
-            
-        out._backward = _backward
-        return out
-    
-    def square(self):
-        """Square of elements"""
-        return self ** 2
-    
-    def clip(self, min_val=None, max_val=None):
-        """Clip values to a range"""
-        clipped = np.clip(self.data, min_val, max_val)
-        return Tensor(clipped, (self,), 'clip')
 
     def __repr__(self):
         return f"Tensor(data={self.data}, grad={self.grad})"
