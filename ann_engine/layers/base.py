763e37683a6d396df9526b4d7aa3b624ce1c5209
biswas-bit <pokhrelbiswas53@gmail.com>
2026-03-01 20:44:13 +0545

    updated parameter

--- ann_engine/layers/base.py
+++ ann_engine/layers/base.py
@@ -1,53 +1,57 @@
-# ann_engine/layers/base.py
 from abc import ABC, abstractmethod
 from ann_engine.core import Parameter
 
+
 class Module(ABC):
     def __init__(self):
-        self._parameters = []
         self.training = True
-    
+
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
-    
+
     @abstractmethod
     def forward(self, *args, **kwargs):
         pass
-    
+
     def parameters(self):
-        """Return list of all parameters in the module"""
+        """
+        Collect all Parameter instances by inspecting instance attributes.
+        Works recursively for nested Modules.
+        """
         params = []
-        for attr in dir(self):
+        seen_ids = set()
+
+        for attr in vars(self):          # ← vars(self) not dir(self)
             obj = getattr(self, attr)
             if isinstance(obj, Parameter):
-                params.append(obj)
+                if id(obj) not in seen_ids:
+                    seen_ids.add(id(obj))
+                    params.append(obj)
             elif isinstance(obj, Module):
-                params.extend(obj.parameters())
+                for p in obj.parameters():
+                    if id(p) not in seen_ids:
+                        seen_ids.add(id(p))
+                        params.append(p)
+
         return params
-    
+
     def train(self):
-        """Set module to training mode"""
+        """Set module to training mode."""
         self.training = True
         for child in self.children():
             child.train()
-    
+
     def eval(self):
-        """Set module to evaluation mode"""
+        """Set module to evaluation mode."""
         self.training = False
         for child in self.children():
             child.eval()
-    
+
     def children(self):
-        """Return child modules"""
+        """Return direct child Modules."""
         children = []
-        for attr in dir(self):
+        for attr in vars(self):
             obj = getattr(self, attr)
             if isinstance(obj, Module):
                 children.append(obj)
         return children
-    
-    def parameters(self):
-        params = list(self._parameters.values())
-        for module in self._modules.values():
-            params.extend(module.parameters())
-        return params
