146be2fff6807ecb82c0ff782d5622f916fc3243
biswas-bit <pokhrelbiswas53@gmail.com>
2026-03-02 19:44:26 +0545

    created a spiral dataset

---
+++ ann_engine/datasets/dataset.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class Datasets:
+    def __init__(self, n=1000, n_classes=3, noise=0.4):
+        self.__n = n  
+        self.__n_classes = n_classes  
+        self.__noise = noise  
+        self.__x = []
+        self.__y = []
+        
+    def make_spiral(self):
+        per_class = self.__n // self.__n_classes
+        for c in range(self.__n_classes):
+            # Fixed: np.linspace, not np.linespace
+            t = np.linspace(0, 1, per_class)
+            angle = t * 4 * np.pi + (2 * np.pi * c / self.__n_classes)
+            r = t
+            # Fixed: noise scaling - multiplied by noise parameter
+            x1 = r * np.cos(angle) + np.random.randn(per_class) * self.__noise
+            x2 = r * np.sin(angle) + np.random.randn(per_class) * self.__noise
+            self.__x.append(np.column_stack([x1, x2]))
+            self.__y.extend([c] * per_class)
+        
+        self.__x = np.vstack(self.__x).astype(np.float32)
+        self.__y = np.eye(self.__n_classes)[np.array(self.__y)].astype(np.float32)
+        idx = np.random.permutation(len(self.__x))
+        return self.__x[idx], self.__y[idx]
